73727e4e5b7d91a829761311c71b975dc98d5fca
Alexander Vo <amv036@ucsd.edu>
2021-02-24 00:12:03 -0800

    Re-organization

--- heap.ts
+++ heap.ts
@@ -1,3 +1,68 @@
+export interface Allocator {
+  alloc: (size: bigint) => Block,
+
+  // NOTE: this probably should take a Block
+  free2: (ptr: bigint) => void,
+
+  owns: (ptr: bigint) => boolean,
+
+  description: () => string,
+}
+
+export interface Block {
+  ptr: bigint,
+  size: bigint,
+}
+
+const NULL_BLOCK: Block = {
+  ptr: 0n,
+  size: 0n,
+};
+
+// NOTE: No deallocation
+// [counter, usableMemory...]
+export class BumpAllocator {
+  counter: bigint;
+  absStart: bigint;
+  absEnd: bigint;
+
+  constructor(s: bigint, endExclusive: bigint) {
+    this.absStart = s;
+    this.counter = s;
+    this.absEnd = endExclusive;
+
+    if (endExclusive <= s) {
+      throw new Error(`Error: end (${endExclusive.toString()})<= start of memory (${s.toString()})`);
+    }
+  }
+
+  alloc(size: bigint): Block {
+    if (this.counter >= this.absEnd - size) {
+      return NULL_BLOCK;
+    }
+
+    const ptr = this.counter;
+    this.counter += size;
+    return {
+      ptr: ptr,
+      size: size,
+    };
+  }
+
+  owns(ptr: bigint): boolean {
+    return ptr >= this.absStart && ptr < this.absEnd;
+  }
+}
+
+// AllocList: [ { header, obj}, { header, obj }, { header, obj }]
+//   header: {
+//      prev: bigint,
+//      next: bigint,
+//   }
+// BitMappedBlocks: [infomap, bucket1, bucket2, bucket3...]
+
+
+
 // flag === true => fallback
 // flag === false => primary
 //
@@ -147,65 +212,4 @@ export class Fallback {
   }
 }
 
-export interface Allocator {
-  alloc: (size: bigint) => Block,
 
-  // NOTE: this probably should take a Block
-  free2: (ptr: bigint) => void,
-
-  owns: (ptr: bigint) => boolean,
-
-  description: () => string,
-}
-
-export interface Block {
-  ptr: bigint,
-  size: bigint,
-}
-
-const NULL_BLOCK: Block = {
-  ptr: 0n,
-  size: 0n,
-};
-
-// NOTE: No deallocation
-// [counter, usableMemory...]
-export class BumpAllocator {
-  counter: bigint;
-  absStart: bigint;
-  absEnd: bigint;
-
-  constructor(s: bigint, endExclusive: bigint) {
-    this.absStart = s;
-    this.counter = s;
-    this.absEnd = endExclusive;
-
-    if (endExclusive <= s) {
-      throw new Error(`Error: end (${endExclusive.toString()})<= start of memory (${s.toString()})`);
-    }
-  }
-
-  alloc(size: bigint): Block {
-    if (this.counter >= this.absEnd - size) {
-      return NULL_BLOCK;
-    }
-
-    const ptr = this.counter;
-    this.counter += size;
-    return {
-      ptr: ptr,
-      size: size,
-    };
-  }
-
-  owns(ptr: bigint): boolean {
-    return ptr >= this.absStart && ptr < this.absEnd;
-  }
-}
-
-// AllocList: [ { header, obj}, { header, obj }, { header, obj }]
-//   header: {
-//      prev: bigint,
-//      next: bigint,
-//   }
-// BitMappedBlocks: [infomap, bucket1, bucket2, bucket3...]
