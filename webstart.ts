2d982f615571c31520c3f3c3d538a3d112b0df0e
Winston Durand <me@winstondurand.com>
2021-02-26 11:03:31 -0800

    INit codemirror

--- webstart.ts
+++ webstart.ts
@@ -3,6 +3,10 @@ import { Type, Value } from './ast';
 import { defaultTypeEnv } from './type-check';
 import { NUM, BOOL, NONE } from './utils';
 
+import CodeMirror from "codemirror"
+import "codemirror/addon/edit/closebrackets"
+import "codemirror/mode/python/python"
+
 function stringify(typ: Type, arg: any) : string {
   switch(typ.tag) {
     case "number":
@@ -105,6 +109,22 @@ function webStart() {
           .catch((e) => { renderError(e); console.log("run failed", e) });;
     });
     setupRepl();
+  });
+
+  window.addEventListener('load', (event) => {
+    const textarea = document.getElementById("user-code") as HTMLTextAreaElement;
+    const editor = CodeMirror.fromTextArea(textarea, {
+        mode: "python",
+        theme: "neo",
+        lineNumbers: true,
+        autoCloseBrackets: true,
+    });
+
+    console.log(editor)
+
+    editor.on("change", (cm, change) => {
+        textarea.value = editor.getValue();
+    })
   });
 }
 
