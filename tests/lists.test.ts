d354c6358da486f4306d75aa7ac108a22edd1640
Chao Chi Cheng <ricky.cheng850504@gmail.com>
2021-02-27 12:16:35 -0800

    update test

--- tests/lists.test.ts
+++ tests/lists.test.ts
@@ -3,17 +3,17 @@ import { assert, asserts, assertPrint } from "./utils.test";
 
 describe('run', () => {
   
-    assert('add', "2 + 3", PyInt(2 + 3));
+    // assert('add', "2 + 3", PyInt(2 + 3));
 
-    assert('Lists Declaration','items : [int] = None\nitems', PyNone())
-    var source = `
-        items : [int] = None
-        n : int = 10
-        items = [1, 2]
-        n = items[0] + items[1]
-        n
-    `
-    assert('Lists Access', source,  PyInt(1 + 2))
+    // assert('Lists Declaration','items : [int] = None\nitems', PyNone())
+    // var source = `
+    //     items : [int] = None
+    //     n : int = 10
+    //     items = [1, 2]
+    //     n = items[0] + items[1]
+    //     n
+    // `
+    // assert('Lists Access', source,  PyInt(1 + 2))
 
 
  
