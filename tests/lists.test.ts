722dd97571db593e3904732a38f26fa31f58039d
Chao Chi Cheng <ricky.cheng850504@gmail.com>
2021-02-28 01:02:25 -0800

    pull

--- tests/lists.test.ts
+++ tests/lists.test.ts
@@ -2,10 +2,14 @@ import { PyInt, PyBool, PyNone, PyObj } from '../utils';
 import { assert, asserts, assertPrint } from "./utils.test";
 
 describe('run', () => {
+    // assert('Parsing Empty List', '[]',  PyNone())
+    // assert('Parsing List', '[1,2,3]',  PyNone())
+    var source = `
+        a : int = 1
+        a = 1
+    `
   
-
-  
-    assert('Parsing Empty List', '[1,2,3]',  PyNone())
+    assert('Parsing Empty List', source,  PyNone())
     // assert('Lists Declaration','items : [int] = None\nitems', PyNone())
     // var source = `
     //     items : [int] = None
