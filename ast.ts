7cb4fe792e572cc398c553e2bdbcf6fac1c6eccb
Chao Chi Cheng <ricky.cheng850504@gmail.com>
2021-02-27 20:39:14 -0800

    ast update

--- ast.ts
+++ ast.ts
@@ -91,14 +91,10 @@ export type Expr<A> =
   | {  a?: A, tag: "lambda", args: Array<string>, ret: Expr<A> }
   | {  a?: A, tag: "comprehension", expr: Expr<A>, field: string, iter: Expr<A>, cond?: Expr<A> }
   | {  a?: A, tag: "block", block: Array<Stmt<A>>, expr: Expr<A> }
-<<<<<<< HEAD
   | {  a?: A, tag: "list-expr", contents: Array<Expr<A>> }
-  | {  a?: A, tag: "bracket-lookup", obj:Expr<A>, key:Expr<A> }
-
-=======
   | {  a?: A, tag: "dict", entries: Array<[Expr<A>, Expr<A>]> }
   | {  a?: A, tag: "bracket-lookup", obj:Expr<A>, key:Expr<A> }
->>>>>>> upstream/main
+
 
 export type Literal = 
     { tag: "num", value: BigInt }
