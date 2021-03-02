0f0ba28d13322e22477495f08f2a96b3bee65c8c
lite-david <edwin.mascarenhas95@gmail.com>
2021-03-01 22:00:46 -0800

    Add parser tests for dict initialization, declaration and bracket-lookup

--- tests/parser.test.ts
+++ tests/parser.test.ts
@@ -1,44 +1,59 @@
-// import * as mocha from 'mocha';
-// import {expect} from 'chai';
-// import { parser } from 'lezer-python';
-// import { traverseExpr, traverseStmt, traverse, parse } from '../parser';
+ import * as mocha from 'mocha';
+ import {expect} from 'chai';
+ import { parser } from 'lezer-python';
+ import { traverseExpr, traverseStmt, traverse, parse, traverseVarInit } from '../parser';
 
-// // We write tests for each function in parser.ts here. Each function gets its
-// // own describe statement. Each it statement represents a single test. You
-// // should write enough unit tests for each function until you are confident
-// // the parser works as expected.
-// describe('traverseExpr(c, s) function', () => {
-//   it('parses a number in the beginning', () => {
-//     const source = "987";
-//     const cursor = parser.parse(source).cursor();
+ describe('parse(source) function', () => {
+   it('parse a typed dict variable initialization', () => {
+     const parsed = parse("d:[int, bool] = None");
+     expect(parsed.inits).to.deep.equal([{
+                                          name: "d",
+                                          type: {
+                                                 tag:"dict",
+                                                 key: {tag:"number"},
+                                                 value: {tag:"bool"}
+                                                 }, //end of type
+                                          value: {tag:"none"}
+                                          }]);
+   });  
 
-//     // go to statement
-//     cursor.firstChild();
-//     // go to expression
-//     cursor.firstChild();
+   it('parse an empty dict expression', () => {
+     const parsed = parse("d = {}");
+     expect(parsed.stmts).to.deep.equal([{
+                                          tag: "assign",
+                                          name: "d",
+                                          value: {
+                                                  tag: "dict",
+                                                  entries: []
+                                                  } // end of values
+                                          }]);
+   });  
+   
+   it('parse a dict expression', () => {
+     const parsed = parse("d = {2:True}");
+     expect(parsed.stmts).to.deep.equal([{
+                                          tag: "assign",
+                                          name: "d",
+                                          value: {
+                                                  tag: "dict",
+                                                  entries: [[{tag:"literal", value: {tag: "num", value: 2n}},
+                                                             {tag:"literal", value: {tag: "bool", value: true}}
+                                                            ]]
+                                                  } // end of values
+                                          }]);
+   });  
+   
+   it('parse a bracket-lookup expression', () => {
+     const parsed = parse("x = d[2]");
+     expect(parsed.stmts).to.deep.equal([{
+                                          tag: "assign",
+                                          name: "x",
+                                          value: {
+                                                  tag: "bracket-lookup",
+                                                  obj: {tag: "id", name: "d"},
+                                                  key: {tag:"literal", value: {tag: "num", value: 2n}}
+                                                  } // end of values
+                                          }]);
+   });  
+});
 
-//     const parsedExpr = traverseExpr(cursor, source);
-
-//     // Note: we have to use deep equality when comparing objects
-//     expect(parsedExpr).to.deep.equal({tag: "num", value: 987});
-//   })
-
-//   // TODO: add additional tests here to ensure traverseExpr works as expected
-// });
-
-// describe('traverseStmt(c, s) function', () => {
-//   // TODO: add tests here to ensure traverseStmt works as expected
-// });
-
-// describe('traverse(c, s) function', () => {
-//   // TODO: add tests here to ensure traverse works as expected
-// });
-
-// describe('parse(source) function', () => {
-//   it('parse a number', () => {
-//     const parsed = parse("987");
-//     expect(parsed).to.deep.equal([{tag: "expr", expr: {tag: "num", value: 987}}]);
-//   });
-
-//   // TODO: add additional tests here to ensure parse works as expected
-// });
