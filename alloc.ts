51cbc3cdb8fe4a69dd18ff479239701ef8ab6905
Alexander Vo <amv036@ucsd.edu>
2021-03-02 02:18:28 -0800

    Update the MemoryManager to use the new GC infrastructure

--- alloc.ts
+++ alloc.ts
@@ -1,16 +1,20 @@
-import * as Heap from "./heap";
+import * as H from "./heap";
 import * as GC from "./gc";
 
 // Untagged pointer (32-bits)
 export type Pointer = bigint;
 
 export class MemoryManager {
-  globalAllocator: Heap.BumpAllocator;
+  globalAllocator: H.BumpAllocator;
+  memory: Uint8Array;
   // In the future, we can do something like
   // globalAllocator: Fallback<BumpAllocator, Generic>
-  gc: GC.MnS;
+  gc: GC.MnS<H.BumpAllocator>;
 
-  constructor(globalStorage: bigint) {
-    this.globalAllocator = new Heap.BumpAllocator(0n, globalStorage);
+  constructor(memory: Uint8Array, globalStorage: bigint, total: bigint) {
+    this.memory = memory;
+    this.globalAllocator = new H.BumpAllocator(memory, 0n, globalStorage);
+    const gcHeap = new H.BumpAllocator(memory, globalStorage, total);
+    this.gc = new GC.MnS(memory, gcHeap);
   }
 }
