56b6b052468d183f81699a56551526e06afa7072
Alexander Vo <amv036@ucsd.edu>
2021-03-02 01:33:32 -0800

    Implement the mark-and-sweep GC skeleton

--- gc.ts
+++ gc.ts
@@ -20,6 +20,26 @@ const HEADER_OFFSET_TAG    = 0x0;
 const HEADER_OFFSET_SIZE   = 0x1;
 const HEADER_OFFSET_GC     = HEADER_OFFSET_TAG + HEADER_OFFSET_SIZE;
 
+// value: 32-bit bigint
+// Uses the least significant bit as a tag:
+//   * `0` => pointer
+//   * `1` => primitive value
+function isPointer(value: bigint): boolean {
+  return Boolean((value & 0x1n) === 0x1n);
+}
+
+// taggedPtr: 32-bit bigint
+// Tagged as a pointer
+//
+// NOTE(alex): NOP
+//   * Assumes that all heap allocations are aligned properly such
+//     that the LSB of all pointers is always `0`
+//
+function extractPointer(taggedPtr: bigint): Pointer {
+  // nop
+  return taggedPtr;
+}
+
 //
 // Proxy for constructed GC object headers
 //
@@ -101,22 +121,171 @@ export interface MarkableAllocator extends H.Allocator {
   // Throws an error if the allocator does not own ptr
   getHeader: (ptr: Pointer) => Header,
 
-  // size: size of object (NOT including header/metadata)
+  // size: size of object in bytes (NOT including header/metadata)
   // tag: heap object tag to know how to traverse the object
   //
-  // Allocators memory of the requested size
+  // Allocates memory of the requested size
   //   May also allocate additional memory to store GC metadata, tag, and size
   //
   // Returns an untagged pointer to the start of the object's memory (not the header)
   // Returns the null pointer (0x0) if memory allocation failed
   gcalloc: (tag: HeapTag, size: bigint) => Pointer,
 
-  collect: () => void,
+  sweep: () => void,
 }
 
-export class MnS {
-  // heap: Segregator<500, Segregator<100, BitMappedBlocks<100>, BitMappedBlocks<500>>, AllocList>;
-  heap: H.Segregator<500n, H.Segregator<100n, H.BumpAllocator, H.BumpAllocator>, H.BumpAllocator>;
+/// Mark-and-sweep GC implementation
+///   * Stop-the-world
+///
+/// Based on pseudo-code from:
+///   The Garbage Collection Handbook: The Art of Automatic Memory Management (Chapman & Hall, 2012
+export class MnS<A extends MarkableAllocator> {
+  memory: Uint8Array;
+  heap: A;
+  roots: Set<Pointer>;
+
+  // roots: array of root pointers to trace
+  //
+  // NOTE(alex): assumes that the caller has already pruned primitive values
+  //   from the list of pointers
+  //
+  addRoot(toAdd: Array<Pointer>) {
+    toAdd.forEach(item => {
+      this.roots.add(item);
+    });
+  }
+
+  // roots: array of root pointers to trace
+  //
+  // NOTE(alex): assumes that the caller has already pruned primitive values
+  //   from the list of pointers
+  removeRoot(toRemove: Array<Pointer>) {
+    toRemove.forEach(item => {
+      this.roots.delete(item);
+    });
+  }
+
+  // Trace the object graph from roots, setting the 'Mark' bit of each reachable object
+  //
+  // NOTE(alex): assumes that the object graph to trace is entirely contained
+  //   by this GC's heap
+  //
+  // NOTE(alex): assumes roots are always valid pointers
+  //
+  // TODO(alex): figure out how to handle marking across GC boundaries
+  markFromRoots() {
+    let worklist: Array<Pointer> = new Array();
+    this.roots.forEach(root => {
+      if (!this.isMarked(root)) {
+        this.setMarked(root);
+        worklist.push(root);
+
+        this.trace(worklist);
+      }
+    });
+  }
+
+  // Trace the object graph based on the worklist
+  // * Depth-first
+  //
+  // NOTE(alex): assumes 32-bit field sizes
+  trace(worklist: Array<Pointer>) {
+    while (worklist.length > 0) {
+      const childPtr = worklist.pop();
+      const headerRef = this.heap.getHeader(childPtr);
+      const childSize = headerRef.getSize();
+      const childTag = headerRef.getTag();
+
+      switch (childTag) {
+        case TAG_CLASS: {
+          for (let fieldIndex = 0n; fieldIndex < childSize / 4n; fieldIndex++) {
+            const fieldValue = this.getField(childPtr + BigInt(4) * fieldIndex);
+            if (!isPointer(fieldValue)) {
+              continue;
+            }
+
+            const fieldPtr = extractPointer(fieldValue);
+            if (!this.isMarked(fieldPtr)) {
+              this.setMarked(fieldPtr);
+              worklist.push(fieldPtr);
+            }
+          }
+          break;
+        }
+
+        case TAG_LIST: {
+          throw new Error("TODO: trace list");
+        }
+
+        case TAG_STRING: {
+          throw new Error("TODO: trace string");
+        }
+
+        case TAG_DICT: {
+          throw new Error("TODO: trace dict");
+        }
+
+        case TAG_BIGINT: {
+          throw new Error("TODO: trace bigint");
+        }
+      }
+
+      throw new Error(`Trying to trace unknown heap object: ${childTag.toString(16)}`);
+    }
+  }
+
+  getField(fieldAddress: Pointer): bigint {
+    let x = BigInt.asUintN(32, 0x0n);
+
+    // WASM stores integers in little-endian:
+    //   LSB at the smallest address
+    for (let i = 0; i < 4; i++) {
+      const b = BigInt(this.memory[Number(fieldAddress) + i]);
+      x = x + (b << BigInt(8 * i));
+    }
+
+    return x;
+  }
+
+  isMarked(child: Pointer): boolean {
+    const headerRef = this.heap.getHeader(child);
+    return headerRef.isMarked();
+  }
+
+  setMarked(child: Pointer) {
+    const headerRef = this.heap.getHeader(child);
+    headerRef.mark();
+  }
+
+  // Traverses the owned heap, freeing objects that are allocated but not marked
+  sweep() {
+    this.heap.sweep();
+  }
+
+  // Try to make space in the heap
+  collect() {
+    this.markFromRoots();
+    this.sweep();
+  }
+
+  // size: size of object in bytes (NOT including header/metadata)
+  // tag: heap object tag to know how to traverse the object
+  //
+  // Allocates memory of the requested size
+  //   * May also allocate additional memory to store GC metadata, tag, and size
+  //   * May pause the user program in order to collect garbage
+  //
+  // Returns an untagged pointer to the start of the object's memory (not the header)
+  // Returns the null pointer (0x0) if memory allocation failed
+  gcalloc(tag: HeapTag, size: bigint): Pointer {
+    let result = this.heap.gcalloc(tag, size);
+    if (result === 0x0n) {
+      this.collect();
+      result = this.heap.gcalloc(tag, size);
+    }
+
+    return result;
+  }
 }
 
 /// ==========================================
@@ -175,9 +344,9 @@ export class MarkableSwitch<P extends MarkableAllocator, F extends MarkableAlloc
     }
   }
 
-  collect(): void {
-    this.allocator.primary.collect();
-    this.allocator.fallback.collect();
+  sweep(): void {
+    this.allocator.primary.sweep();
+    this.allocator.fallback.sweep();
   }
 }
 
@@ -224,9 +393,9 @@ export class MarkableSegregator<N extends bigint, S extends MarkableAllocator, L
     }
   }
 
-  collect(): void {
-    this.allocator.small.collect();
-    this.allocator.large.collect();
+  sweep(): void {
+    this.allocator.small.sweep();
+    this.allocator.large.sweep();
   }
 }
 
@@ -264,8 +433,8 @@ export class MarkableDescriber<A extends MarkableAllocator> implements MarkableA
     return this.allocator.allocator.gcalloc(tag, size);
   }
 
-  collect(): void {
-    this.allocator.allocator.collect();
+  sweep(): void {
+    this.allocator.allocator.sweep();
   }
 }
 
@@ -314,8 +483,8 @@ export class MarkableFallback<P extends MarkableAllocator, F extends MarkableAll
     return b1;
   }
 
-  collect(): void {
-    this.allocator.primary.collect();
-    this.allocator.fallback.collect();
+  sweep(): void {
+    this.allocator.primary.sweep();
+    this.allocator.fallback.sweep();
   }
 }
