0c3260e7b4f624979143d9be3b16c62dde30e69c
Shanli Yu <shy118@ucsd.edu>
2021-03-02 21:39:29 -0800

    C2PFE-12: Moved hardcoded theme list to an external file for import

--- webstart.ts
+++ webstart.ts
@@ -1,6 +1,7 @@
 import { BasicREPL } from "./repl";
 import { Type, Value } from "./ast";
 import { NUM, BOOL, NONE, unhandledTag } from "./utils";
+import { themeList_export } from "./themelist"
 
 import CodeMirror from "codemirror"
 import "codemirror/addon/edit/closebrackets"
@@ -148,70 +149,7 @@ function webStart() {
   });
 
   window.addEventListener('load', (event) => {
-    const themeList = ["3024-day",
-    "3024-night",
-    "abcdef",
-    "ambiance-mobile",
-    "ambiance",
-    "ayu-dark",
-    "ayu-mirage",
-    "base16-dark",
-    "base16-light",
-    "bespin",
-    "blackboard",
-    "cobalt",
-    "colorforth",
-    "darcula",
-    "dracula",
-    "duotone-dark",
-    "duotone-light",
-    "eclipse",
-    "elegant",
-    "erlang-dark",
-    "gruvbox-dark",
-    "hopscotch",
-    "icecoder",
-    "idea",
-    "isotope",
-    "lesser-dark",
-    "liquibyte",
-    "lucario",
-    "material-darker",
-    "material-ocean",
-    "material-palenight",
-    "material",
-    "mbo",
-    "mdn-like",
-    "midnight",
-    "monokai",
-    "moxer",
-    "neat",
-    "neo",
-    "night",
-    "nord",
-    "oceanic-next",
-    "panda-syntax",
-    "paraiso-dark",
-    "paraiso-light",
-    "pastel-on-dark",
-    "railscasts",
-    "rubyblue",
-    "seti",
-    "shadowfox",
-    "solarized",
-    "ssms",
-    "the-matrix",
-    "tomorrow-night-bright",
-    "tomorrow-night-eighties",
-    "ttcn",
-    "twilight",
-    "vibrant-ink",
-    "xq-dark",
-    "xq-light",
-    "yeti",
-    "yonce",
-    "zenburn"];
-
+    const themeList = themeList_export;
     const dropdown = document.createElement("select");
     dropdown.setAttribute("class", "theme-dropdown");
     dropdown.setAttribute("id", "theme-dropdown");
