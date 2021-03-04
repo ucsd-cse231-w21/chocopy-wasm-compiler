21ae280ddd8dcb875fc8a9aff96628c36c6cff9b
Subhash Ramesh <subby@Subhashs-MacBook-Pro.local>
2021-03-04 15:35:06 -0800

    Added more tests for checking expression evaluation in comprehensions

--- tests/comprehensions.test.ts
+++ tests/comprehensions.test.ts
@@ -2,7 +2,7 @@ import { assertPrint } from "./utils.test";
 
 describe("comprehensions test", () => {
   assertPrint(
-    "Empty comprehension",
+    "Always false comprehension",
     `
     a: Range = None
     a = [i for i in range(0,5) if False]
@@ -19,6 +19,44 @@ describe("comprehensions test", () => {
     a = [i for i in range(0,5)]
     while a.has_next():
       print(a.next())
+    `,
+    ["0", "1", "2", "3", "4"]
+  );
+
+  assertPrint(
+    "Multiple comprehensions",
+    `
+    a: Range = None
+    b: Range = None
+
+    a = [i for i in range(0,5)]
+    while a.has_next():
+      print(a.next())
+
+    b = [i for i in range(10,15)]
+    while b.has_next():
+      print(b.next())
+    `,
+    ["0", "1", "2", "3", "4", "10", "11", "12", "13", "14"]
+  );
+
+  // TODO: These tests should pass once the expression in the comprehensions are actually evaluated.
+  assertPrint(
+    "Expression comprehension",
+    `
+    a: Range = None
+    a = [i * 4 for i in range(0,5)]
+    while a.has_next():
+      print(a.next())
+    `,
+    ["0", "4", "8", "12", "16"]
+  );
+
+  assertPrint(
+    "Function expression comprehensions",
+    `
+    a: Range = None
+    a = [print(i) for i in range(0, 5)]
     `,
     ["0", "1", "2", "3", "4"]
   );
