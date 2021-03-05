d9f1740f31ecc686f8c6bc28d15bd86e477c735d
Beichen Lyu <unbenson.lyu@gmail.com>
2021-03-04 23:52:56 -0800

    import alias should be required

--- ast.ts
+++ ast.ts
@@ -65,7 +65,7 @@ export type Stmt<A> =
   | { a?: A; tag: "break" }
   | { a?: A; tag: "for"; name: string; index?: Expr<A>; iterable: Expr<A>; body: Array<Stmt<A>> }
   | { a?: A; tag: "bracket-assign"; obj: Expr<A>; key: Expr<A>; value: Expr<A> }
-  | { a?: A, tag: "import", isFromStmt:boolean, target: string, compName?: string, alias?: string}; // by Jose
+  | { a?: A, tag: "import", isFromStmt:boolean, target: string, compName?: string, alias: string}; // by Jose
 
 export interface Destructure<A> {
   isDestructured: boolean;
