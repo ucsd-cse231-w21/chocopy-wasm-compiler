4a2635025307b43bd37ee4803ff45217709deae0
Erika Auyeung <eauyeung@ucsd.edu>
2021-03-05 05:36:15 +0000

    Improve tests

--- tests/lists.test.ts
+++ tests/lists.test.ts
@@ -2,12 +2,15 @@ import { PyInt, PyBool, PyNone, PyObj } from "../utils";
 import { assert, asserts, assertPrint } from "./utils.test";
 
 describe("LIST TEST", () => {
+  //Programs described in the writeup, with additional changes
+  //as necessary to actually test functionality
   var source = `
         items : [int] = None
-        items = [1, 2, 3]
+        items = [1, 2, 3, 4, 5, 6, 7, 8]
+        items[7]
     `;
 
-  assert("Assign List To Variable (Program 2)", source, PyNone());
+  assert("Program 2: Assign List To Variable", source, PyInt(8));
 
   var source = `
     items : [int] = None
@@ -15,7 +18,7 @@ describe("LIST TEST", () => {
     items[5]
     `;
 
-  assert("Concat List (Program 3)", source, PyInt(6));
+  assert("Program 3: Concat Lists", source, PyInt(6));
 
   var source = `
         items : [int] = None
@@ -25,9 +28,9 @@ describe("LIST TEST", () => {
         n
     `;
 
-  assert("Lists Access (Program 4)", source, PyInt(1 + 2));
+  assert("Program 4: Lists Access", source, PyInt(1 + 2));
 
-  //Other Test
+  //Other Tests
   assert("Empty List", "[]", PyObj(`list<none>`, 540));
   assert("List With Number", "[1,2,3]", PyObj(`list<number>`, 632));
 
@@ -41,7 +44,18 @@ describe("LIST TEST", () => {
         x2 : A = None
         x3 : A = None  
         x = [A(),A(),A()]
+        x[1].n
         `;
+  assert("List With Class", source, PyInt(100));
 
-  assert("List With Class", source, PyNone());
+  var source = `
+    items : [bool] = None
+    stuff : [bool] = None
+    concatted : [bool] = None
+    items = [True, True, False]
+    stuff = [False, True]
+    concatted = items + stuff
+    concatted[4]
+  `;
+  assert("Concat List Vars", source, PyBool(true));
 });
