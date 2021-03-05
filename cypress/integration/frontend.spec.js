0f721549327690c9c363f4ce2362ed8164d13748
Edward Chen <edwjchen@gmail.com>
2021-03-04 22:22:36 -0800

    adding in new cypress formatted test

--- cypress/integration/frontend.spec.js
+++ cypress/integration/frontend.spec.js
@@ -1,7 +1,7 @@
-Cypress.on('uncaught:exception', (err, runnable) => {
-  expect(err.message).to.include("wabt is not defined")
-  return false
-})
+Cypress.on("uncaught:exception", (err, runnable) => {
+  expect(err.message).to.include("wabt is not defined");
+  return false;
+});
 
 describe("Frontend Tests", () => {
   beforeEach(() => {
@@ -9,14 +9,16 @@ describe("Frontend Tests", () => {
   });
 
   it("Simple Test Code Mirror", () => {
-    cy.get('div[class="CodeMirror cm-s-neo"]').click().find('textarea', { force: true })
-      .type("x:int = 0\nx\n", { force: true })
-    cy.get('button[id="run"]').click() 
-    cy.get('div[id="output"]').contains('pre', '0')
+    cy.get('div[class="CodeMirror cm-s-neo"]')
+      .click()
+      .find("textarea", { force: true })
+      .type("x:int = 0\nx\n", { force: true });
+    cy.get('button[id="run"]').click();
+    cy.get('div[id="output"]').contains("pre", "0");
   });
 
   it("Simple Test Repl", () => {
-    cy.get('textarea[id="next-code"]').type("x:int = 0\nx\n")
-    cy.get('div[id="output"]').contains('pre', '0')
+    cy.get('textarea[id="next-code"]').type("x:int = 0\nx\n");
+    cy.get('div[id="output"]').contains("pre", "0");
   });
 });
