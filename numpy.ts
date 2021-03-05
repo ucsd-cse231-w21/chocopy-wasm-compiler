c6ea8dbb2d765cc0459febfab07053c9322f4d95
Beichen Lyu <unbenson.lyu@gmail.com>
2021-03-04 16:34:05 -0800

    fixed bigint; added dot() method

--- numpy.ts
+++ numpy.ts
@@ -20,21 +20,22 @@ import {
 export const numpyFields : Array<VarInit<null>> = [
 	{name: "dtype", type: {tag: "string"}, value: { tag: "string", value: "int32"} }, // assume int32
 	// use shape0/shape1 for now; wait for ast update on list value type
-	{name: "shape0", type: {tag: "number"}, value: { tag: "num", value: -1} },
-	{name: "shape1", type: {tag: "number"}, value: { tag: "num", value: -1} }
+	{name: "shape0", type: {tag: "number"}, value: { tag: "num", value: BigInt(-1)} },
+	{name: "shape1", type: {tag: "number"}, value: { tag: "num", value: BigInt(-1)} }
 	// {name: "shape", type: {tag: "list", content_type: {tag: "number"} }, value: { tag: "none"} } // use list as tuple
 	// skip other fields e.g. stride, offset
 ];
 
 export const numpyMethods : Array<FunDef<null>> = [
 	// method signature only; special codegen in compiler.ts
-	{name: "__add__", // x1.__add__(x2)
+	{name: "__add__", // self.__add__(x2), element-wise add
 	parameters: [{name: "x2", type: {tag: "class", name: "numpy"}}], 
 	ret: {tag: "class", name: "numpy"},
-	decls: [],
-	inits: [],
-	funs: [],
-	body: []}
+	decls: [], inits: [], funs: [], body: []},
+	{name: "dot", // self.dot(x2) or self @ x2, matrix multiplication
+	parameters: [{name: "x2", type: {tag: "class", name: "numpy"}}], 
+	ret: {tag: "class", name: "numpy"},
+	decls: [], inits: [], funs: [], body: []}
 ];
 
 
