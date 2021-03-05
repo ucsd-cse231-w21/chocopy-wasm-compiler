9db5ecbdb630ee9c5a8b1e0e40c565cc8687a9f0
Edward Chen <edwjchen@gmail.com>
2021-03-04 18:08:45 -0800

    added simple cypress test

--- cypress/integration/frontend.spec.js
+++ cypress/integration/frontend.spec.js
@@ -1,12 +1,18 @@
-describe("Frontend Tests", () => {  
-    beforeEach(() => {
-      cy.exec('npm start')
-      cy.visit("/");
-    });
-
-    it("Sample Test", () => {
-       console.log("Running sample test") 
-        cy.get('textarea[id="user-code"]').type("x:int = 0\nx")
-        cy.get('button[id="run"]').click()
-    });
+describe("Frontend Tests", () => {
+  beforeEach(() => {
+    cy.visit("/");
+    cy.on('uncaught:exception', (err, runnable) => {
+      expect(err.message).to.include("wabt is not defined")
+      return false
+    })
+  
   });
+
+  it("Sample Test", () => {
+    console.log("Running sample test")
+    cy.get('textarea[id="next-code"]').type("x:int = 0\nx\n")
+  });
+});
+
+    // cy.get('div[class="CodeMirror cm-s-neo"]')
+    // .type("x:int = 0\nx")
