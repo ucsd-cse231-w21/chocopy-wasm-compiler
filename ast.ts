c7ab435193128bb7814bd8c0f56f564529d368c4
Daniel Tan <danielytan1@gmail.com>
2021-03-08 21:49:18 -0800

    added argument type to ast

--- ast.ts
+++ ast.ts
@@ -24,6 +24,8 @@ export type Scope<A> =
 
 export type Parameter = { name: string; type: Type; value?: Literal };
 
+export type Argument<A> = { a?: A; name?: string; value: Expr<A> }
+
 export type Program<A> = {
   a?: A;
   funs: Array<FunDef<A>>;
