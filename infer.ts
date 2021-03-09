5f8a74e9134b2b6802cf59471f6fbe1b6a2b917e
Mac Lee <macthecadillac@gmail.com>
2021-03-08 21:47:27 -0800

    `isSubtype` now based on row polymorphism. Scratch version.

--- infer.ts
+++ infer.ts
@@ -8,7 +8,6 @@ import {
   emptyGlobalTypeEnv,
   emptyLocalTypeEnv,
   isNoneOrClass,
-  isSubtype,
   GlobalTypeEnv,
   LocalTypeEnv
 } from "./type-check";
@@ -41,6 +40,69 @@ export function inferTypeLit(lit: Literal): Type {
       return NUM;
     case "string":
       return STRING;
+  }
+}
+
+// check if t1 is a subtype of t2
+export function isSubtype(t1: Type, t2: Type, globEnv: GlobalTypeEnv): boolean {
+  if (t1.tag == "none") {
+    return t2.tag == "class";
+  } else if (t1.tag != "class" || t2.tag != "class") {
+    return t1.tag == t2.tag
+  } else if (!globEnv.classes.has(t1.name)) {
+    throw new Error(`Unknown class: ${t1.name}`);
+  } else if (!globEnv.classes.has(t2.name)) {
+    throw new Error(`Unknown class: ${t2.name}`);
+  } else {
+    // get stuff
+    const t1members = globEnv.classes.get(t1.name);
+    const t2members = globEnv.classes.get(t2.name);
+    const t1fields = t1members[0];
+    const t2fields = t2members[0];
+    const t1methods = t1members[1];
+    const t2methods = t2members[1];
+
+    // Check that t1fields is a subset of t2fields. We do this by iterating
+    // through all the fields of t1 and check that t2 contains every one of
+    // them.
+    for (const [field, left] of t1fields.entries()) {
+      if (!t2fields.has(field)) {
+        return false
+      }
+      const right = t2fields.get(field);
+      // Afterward, we check that the type of each corresponding field in t1 is
+      // of a subtype of its t2 counterpart. FIXME: this is the one thing I'm
+      // not 100% sure about
+      if (!isSubtype(left, right, globEnv)) {
+        return false
+      }
+    }
+
+    // Check that t1methods is a subset of t2methods. We do this by iterating
+    // through all the methods of t1 and check that t2 contains every one of
+    // them
+    for (const [method, [argTypes1, retType1]] of t2methods.entries()) {
+      if (!t2methods.has(method)) {
+        return false
+      }
+      // Afterward, we check that the arguement types of each corresponding
+      // method in t1 is of a subtype of that of its t2 counterpart. FIXME:
+      // ditto above
+      const [argTypes2, retType2] = t2methods.get(method);
+      for (const [left, right] of argTypes1.map((t, i) => [t, argTypes2[i]])) {
+        if (!isSubtype(left, right, globEnv)) {
+          return false
+        }
+      }
+      // We also check that the return type of t1 is a subtype of that of t2.
+      // FIXME: ditto above
+      if (!isSubtype(retType1, retType2, globEnv)) {
+        return false
+      }
+    }
+
+    // if there hasn't been an early return, t1 is a subtrype of t2
+    return true
   }
 }
 
@@ -301,7 +363,7 @@ export function buildTypedStmt(
         let type_ = locEnv.vars.get(stmt.name);
         // FIXME: make isSubtype work with LocalTypeEnv since this is a local
         // variable we are talking about
-        if (!isSubtype(globEnv, a, type_)) {
+        if (!isSubtype(type_, a, globEnv)) {
           a = UNSAT;
         }
       } else {
@@ -338,7 +400,7 @@ export function buildTypedStmt(
           || obj.a.tag != "class"
           || !globEnv.classes.has(obj.a.name)  // if class map contains the class
           || !globEnv.classes.get(obj.a.name)[0].has(stmt.field) // if the class contains the field
-          || !isSubtype(globEnv, globEnv.classes.get(obj.a.name)[0].get(stmt.field), value.a)) {
+          || !isSubtype(globEnv.classes.get(obj.a.name)[0].get(stmt.field), value.a, globEnv)) {
           a = UNSAT;
       } else {
         a = globEnv.classes.get(obj.a.name)[0].get(stmt.field);
