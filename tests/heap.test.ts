017db7d28e1b73711e274426829d785be27359fb
Alexander Vo <amv036@ucsd.edu>
2021-03-10 00:28:30 -0800

    Update heap tests to check expected pointer values

--- tests/heap.test.ts
+++ tests/heap.test.ts
@@ -21,11 +21,16 @@ describe("Heap", () => {
       it("Should initialize with correct number of blocks", () => {
         expect(bmb.getNumFreeBlocks()).to.eq(90);
       })
-  
+
       it("Should allocate appropriate number of blocks", () => {
-        const ptr = bmb.gcalloc(TAG_CLASS, 25n);
+        const ptr1 = bmb.gcalloc(TAG_CLASS, 25n);
         // Three blocks are allocated for request of size 25
+        expect(Number(ptr1)).to.eq(100);
         expect(bmb.getNumFreeBlocks()).to.eq(87);
+
+        const ptr2 = bmb.gcalloc(TAG_CLASS, 25n);
+        expect(Number(ptr2)).to.eq(130);
+        expect(bmb.getNumFreeBlocks()).to.eq(84);
       });
 
       it("Should return an invalid pointer if request exceeds available memory", () => {
