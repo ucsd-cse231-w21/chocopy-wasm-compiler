010310552c6722ce8b51b82aa81ff0d1fd305cde
Alexander Vo <amv036@ucsd.edu>
2021-03-10 02:53:32 -0800

    Add failing heap allocation pattern test for BitMappedBlocks

--- tests/heap.test.ts
+++ tests/heap.test.ts
@@ -1,12 +1,57 @@
 import "mocha";
 import { expect } from "chai";
 import { BitMappedBlocks } from "../heap";
-import {TAG_CLASS} from "../gc";
+import {TAG_CLASS, TAG_REF, HEADER_SIZE_BYTES} from "../gc";
 
 describe("Heap", () => {
 
   // Unit tests for BitMappedBlock heap implementation
   describe("BitMappedBlock", () => {
+
+    // Problematic allocation requests
+    // Based on failing tests/programs
+    describe("Problematic alloc pattern", () => {
+      it("Closure test 7 alloc pattern", () => {
+        const bmb = new BitMappedBlocks(516n, 2000n, 4n, BigInt(HEADER_SIZE_BYTES));
+
+        const ptr0 = bmb.gcalloc(TAG_REF, 4n);
+        expect(bmb.infomap[0]).to.eq(1);
+        expect(Number(ptr0)).to.eq(516);
+
+        const ptr1 = bmb.gcalloc(TAG_REF, 4n);
+        expect(bmb.infomap[0]).to.eq(1);
+        expect(bmb.infomap[9]).to.eq(1);
+        expect(Number(ptr1)).to.eq(520);
+
+        const ptr2 = bmb.gcalloc(TAG_REF, 4n);
+        expect(bmb.infomap[0]).to.eq(1);
+        expect(bmb.infomap[9]).to.eq(1);
+        expect(bmb.infomap[18]).to.eq(1);
+        expect(Number(ptr2)).to.eq(524);
+
+        const ptr3 = bmb.gcalloc(TAG_REF, 4n);
+        expect(bmb.infomap[0]).to.eq(1);
+        expect(bmb.infomap[9]).to.eq(1);
+        expect(bmb.infomap[18]).to.eq(1);
+        expect(bmb.infomap[27]).to.eq(1);
+        expect(Number(ptr3)).to.eq(528);
+
+        const ptr4 = bmb.gcalloc(TAG_REF, 4n);
+        expect(bmb.infomap[0]).to.eq(1);
+        expect(bmb.infomap[9]).to.eq(1);
+        expect(bmb.infomap[18]).to.eq(1);
+        expect(bmb.infomap[27]).to.eq(1);
+        expect(bmb.infomap[36]).to.eq(1);
+        expect(Number(ptr4)).to.eq(532);
+
+        const ptr5 = bmb.gcalloc(TAG_REF, 4n);
+        expect(Number(ptr5)).to.eq(536);
+
+        const ptr6 = bmb.gcalloc(TAG_REF, 4n);
+        expect(Number(ptr6)).to.eq(540);
+      });
+    });
+
 
     // Test for block allocation strategy
     describe("Number of blocks", () => {
@@ -63,4 +108,4 @@ describe("Heap", () => {
       });
     });
   });
-})
+});
