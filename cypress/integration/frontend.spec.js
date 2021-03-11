04d4c66d87c64ef4cde36fcccfc9f54b040456a4
Shanli Yu <shy118@ucsd.edu>
2021-03-11 13:31:45 -0800

    Fixing formatting issues

--- cypress/integration/frontend.spec.js
+++ cypress/integration/frontend.spec.js
@@ -27,20 +27,20 @@ describe("Frontend Tests", () => {
       .click()
       .find("textarea", { force: true })
       .type("class Rat(object):\na:int=1\n", { force: true })
-      .type('{backspace}{backspace}{backspace}{backspace}')
-      .type("x : Rat = None\n{backspace}{backspace}x = Rat()\nx", { force: true });      
+      .type("{backspace}{backspace}{backspace}{backspace}")
+      .type("x : Rat = None\n{backspace}{backspace}x = Rat()\nx", { force: true });
     cy.get('button[id="run"]').click();
     cy.get('button[class="accordion"]').click();
     cy.get('div[id="output"]').contains('b[class="tag"]', "a");
   });
-  
+
   it("Pretty print nested objects", () => {
     cy.get('div[class="CodeMirror cm-s-neo CodeMirror-simplescroll"]')
       .click()
       .find("textarea", { force: true })
       .type("class Rat(object):\na:int=1\n{backspace}{backspace}b:Rat=None\n", { force: true })
-      .type('{backspace}{backspace}{backspace}{backspace}')
-      .type("x : Rat = None\n{backspace}{backspace}x = Rat()\nx.b=Rat()\nx.b", { force: true });      
+      .type("{backspace}{backspace}{backspace}{backspace}")
+      .type("x : Rat = None\n{backspace}{backspace}x = Rat()\nx.b=Rat()\nx.b", { force: true });
     cy.get('button[id="run"]').click();
     cy.get('button[class="accordion"]').click();
     cy.get('div[id="output"]').contains('b[class="tag"]', "a");
@@ -51,10 +51,10 @@ describe("Frontend Tests", () => {
       .click()
       .find("textarea", { force: true })
       .type("class Rat(object):\na:int=1\n{backspace}{backspace}b:[int]=None\n", { force: true })
-      .type('{backspace}{backspace}{backspace}{backspace}')
-      .type("x : Rat = None\n{backspace}{backspace}x = Rat()\nx.b=[1,2,3,4]\nx", { force: true });      
+      .type("{backspace}{backspace}{backspace}{backspace}")
+      .type("x : Rat = None\n{backspace}{backspace}x = Rat()\nx.b=[1,2,3,4]\nx", { force: true });
     cy.get('button[id="run"]').click();
-    cy.get('button[class="accordion"]').click({multiple: true});
+    cy.get('button[class="accordion"]').click({ multiple: true });
     cy.get('div[id="output"]').contains('b[class="tag"]', "3");
   });
 
@@ -62,11 +62,15 @@ describe("Frontend Tests", () => {
     cy.get('div[class="CodeMirror cm-s-neo CodeMirror-simplescroll"]')
       .click()
       .find("textarea", { force: true })
-      .type("class Rat(object):\na:int=1\n{backspace}{backspace}b:[int,int]=None\n", { force: true })
-      .type('{backspace}{backspace}{backspace}{backspace}')
-      .type("x : Rat = None\n{backspace}{backspace}x = Rat()\nx.b={{}1:2, 3:4{}}\nx", { force: true });      
+      .type("class Rat(object):\na:int=1\n{backspace}{backspace}b:[int,int]=None\n", {
+        force: true,
+      })
+      .type("{backspace}{backspace}{backspace}{backspace}")
+      .type("x : Rat = None\n{backspace}{backspace}x = Rat()\nx.b={{}1:2, 3:4{}}\nx", {
+        force: true,
+      });
     cy.get('button[id="run"]').click();
-    cy.get('button[class="accordion"]').click({multiple: true});
+    cy.get('button[class="accordion"]').click({ multiple: true });
     cy.get('div[id="output"]').contains('b[class="tag"]', "3");
   });
 
@@ -75,7 +79,7 @@ describe("Frontend Tests", () => {
       .click()
       .find("textarea", { force: true })
       .type("a : [int] = None\n{backspace}{backspace}", { force: true })
-      .type("a = [1,2,3,4]\na", { force: true });      
+      .type("a = [1,2,3,4]\na", { force: true });
     cy.get('button[id="run"]').click();
     cy.get('button[class="accordion"]').click();
     cy.get('div[id="output"]').contains('b[class="tag"]', "1");
@@ -86,9 +90,9 @@ describe("Frontend Tests", () => {
       .click()
       .find("textarea", { force: true })
       .type("a : [[int]] = None\n{backspace}{backspace}", { force: true })
-      .type("a = [[1],[2]]\na", { force: true });      
+      .type("a = [[1],[2]]\na", { force: true });
     cy.get('button[id="run"]').click();
-    cy.get('button[class="accordion"]').click({multiple: true});
+    cy.get('button[class="accordion"]').click({ multiple: true });
     cy.get('div[id="output"]').contains('b[class="tag"]', "0");
     cy.get('div[id="output"]').contains('p[class="val"]', "1");
     cy.get('div[id="output"]').contains('b[class="tag"]', "1");
@@ -100,11 +104,11 @@ describe("Frontend Tests", () => {
       .click()
       .find("textarea", { force: true })
       .type("class Rat(object):\na:int=1\n", { force: true })
-      .type('{backspace}{backspace}{backspace}{backspace}')
+      .type("{backspace}{backspace}{backspace}{backspace}")
       .type("a : [Rat] = None\n{backspace}{backspace}", { force: true })
-      .type("a = [Rat(), Rat()]\na", { force: true });      
+      .type("a = [Rat(), Rat()]\na", { force: true });
     cy.get('button[id="run"]').click();
-    cy.get('button[class="accordion"]').click({multiple: true});
+    cy.get('button[class="accordion"]').click({ multiple: true });
     cy.get('div[id="output"]').contains('b[class="tag"]', "a");
     cy.get('div[id="output"]').contains('p[class="val"]', "1");
   });
@@ -114,9 +118,9 @@ describe("Frontend Tests", () => {
       .click()
       .find("textarea", { force: true })
       .type("a : [[int,int]] = None\n{backspace}{backspace}", { force: true })
-      .type("a = [{{}1:2{}},{{}3:4{}}]\na", { force: true });      
+      .type("a = [{{}1:2{}},{{}3:4{}}]\na", { force: true });
     cy.get('button[id="run"]').click();
-    cy.get('button[class="accordion"]').click({multiple: true});
+    cy.get('button[class="accordion"]').click({ multiple: true });
     cy.get('div[id="output"]').contains('b[class="tag"]', "1");
     cy.get('div[id="output"]').contains('p[class="val"]', "2");
     cy.get('div[id="output"]').contains('b[class="tag"]', "3");
@@ -128,7 +132,7 @@ describe("Frontend Tests", () => {
       .click()
       .find("textarea", { force: true })
       .type("a : [int,int] = None\n{backspace}{backspace}", { force: true })
-      .type("a = {{}1:2, 3:4{}}\na", { force: true });      
+      .type("a = {{}1:2, 3:4{}}\na", { force: true });
     cy.get('button[id="run"]').click();
     cy.get('button[class="accordion"]').click();
     cy.get('div[id="output"]').contains('b[class="tag"]', "1");
@@ -139,9 +143,9 @@ describe("Frontend Tests", () => {
       .click()
       .find("textarea", { force: true })
       .type("a : [int,[int,bool]] = None\n{backspace}{backspace}", { force: true })
-      .type("a = {{}1:{{}2:True{}}, 3:{{}4:False{}} {}}\na", { force: true });      
+      .type("a = {{}1:{{}2:True{}}, 3:{{}4:False{}} {}}\na", { force: true });
     cy.get('button[id="run"]').click();
-    cy.get('button[class="accordion"]').click({multiple: true});
+    cy.get('button[class="accordion"]').click({ multiple: true });
     cy.get('div[id="output"]').contains('b[class="tag"]', "2");
     cy.get('div[id="output"]').contains('p[class="val"]', "true");
     cy.get('div[id="output"]').contains('b[class="tag"]', "4");
@@ -152,27 +156,27 @@ describe("Frontend Tests", () => {
       .click()
       .find("textarea", { force: true })
       .type("class Rat(object):\nb:int=1\n", { force: true })
-      .type('{backspace}{backspace}{backspace}{backspace}')
+      .type("{backspace}{backspace}{backspace}{backspace}")
       .type("a : [int,Rat] = None\n{backspace}{backspace}", { force: true })
-      .type("a = {{} 1:Rat(), 2: Rat() {}}\n", { force: true }) 
+      .type("a = {{} 1:Rat(), 2: Rat() {}}\n", { force: true })
       .type("a[1].b=4\n", { force: true })
       .type("a", { force: true });
     cy.get('button[id="run"]').click();
-    cy.get('button[class="accordion"]').click({multiple: true});
+    cy.get('button[class="accordion"]').click({ multiple: true });
     cy.get('div[id="output"]').contains('b[class="tag"]', "b");
     cy.get('div[id="output"]').contains('p[class="val"]', "4");
     cy.get('div[id="output"]').contains('b[class="tag"]', "b");
     cy.get('div[id="output"]').contains('p[class="val"]', "1");
   });
 
-    it("Pretty print dict of lists", () => {
+  it("Pretty print dict of lists", () => {
     cy.get('div[class="CodeMirror cm-s-neo CodeMirror-simplescroll"]')
       .click()
       .find("textarea", { force: true })
       .type("a : [int,[bool]] = None\n{backspace}{backspace}", { force: true })
-      .type("a = {{}1:[True, True], 3:[False, False, False, False] {}}\na", { force: true });      
+      .type("a = {{}1:[True, True], 3:[False, False, False, False] {}}\na", { force: true });
     cy.get('button[id="run"]').click();
-    cy.get('button[class="accordion"]').click({multiple: true});
+    cy.get('button[class="accordion"]').click({ multiple: true });
     cy.get('div[id="output"]').contains('b[class="tag"]', "0");
     cy.get('div[id="output"]').contains('p[class="val"]', "true");
     cy.get('div[id="output"]').contains('b[class="tag"]', "3");
