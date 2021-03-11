db64fdba498bbc0fc4b7c8fc3e7870df5da97574
Alexander Vo <amv036@ucsd.edu>
2021-03-10 18:46:14 -0800

    Make basic MnS tests re-usable with different heap allocators

--- tests/gc-unit.test.ts
+++ tests/gc-unit.test.ts
@@ -134,224 +134,247 @@ function expectFreeHeader(header: Header, tag: HeapTag, size: bigint) {
   expect(Number(header.getTag())).to.equal(Number(tag));
 }
 
+type Cfg = {
+  heap: PhantomAllocator,
+  memory: Uint8Array,
+  name: string,
+}
+
 describe("MnS", () => {
   describe("MnS-BumpAllocator-1", () => {
-    let memory: Uint8Array;
-    let heap: PhantomAllocator;
 
-    beforeEach(() => {
-      memory = new Uint8Array(512);
+    function makeCfg(): Cfg {
+      const memory = new Uint8Array(512);
       const bmb = new BitMappedBlocks(100n, 200n, 4n, BigInt(HEADER_SIZE_BYTES));
-      heap = new PhantomAllocator(bmb);
-    });
+      const heap = new PhantomAllocator(bmb);
 
-    // Simulating:
-    // class C:
-    //   f: int
-    //
-    // def x():
-    //   x = C()
-    //   x = C()
-    //   y = C()
-    //
-    // f()
-    it("local variable class allocate and sweep", () => {
-      const mns = new MnS(memory, heap);
+      return {
+        heap: heap,
+        memory: memory,
+        name: "BitMappedBlocks",
+      };
+    }
 
-      mns.roots.pushFrame();
-      const ptr0 = mns.gcalloc(TAG_CLASS, 4n);
-      expect(Number(ptr0)).to.equal(100);
-      mns.roots.addLocal(0n, ptr0);
-
-      const ptr1 = mns.gcalloc(TAG_CLASS, 4n);
-      expect(Number(ptr1)).to.equal(104);
-      mns.roots.addLocal(0n, ptr1);
-
-      const ptr2 = mns.gcalloc(TAG_CLASS, 4n);
-      expect(Number(ptr2)).to.equal(108);
-      mns.roots.addLocal(1n, ptr2);
-
-      // Check that headers set correctly
-      {
-        const headers = [
-          heap.mappedHeader(ptr0),
-          heap.mappedHeader(ptr1),
-          heap.mappedHeader(ptr2)
-        ];
-        headers.forEach((h, index) => {
-          console.log(`Checking header: ${index}...`);
-          expectAllocatedHeader(h, TAG_CLASS, 4n);
-        });
-      }
-
-      mns.collect();
-
-      // Check that only ptr0 is freed
-      {
-        const header0 = heap.mappedHeader(ptr0);
-        const header1 = heap.mappedHeader(ptr1);
-        const header2 = heap.mappedHeader(ptr2);
-
-        expectFreeHeader(header0, TAG_CLASS, 4n);
-        expectAllocatedHeader(header1, TAG_CLASS, 4n);
-        expectAllocatedHeader(header2, TAG_CLASS, 4n);
-      }
-
-      mns.roots.releaseLocals();
-      mns.collect();
-      // Check that ptr0, ptr1, ptr2 is freed
-      {
-        const headers = [
-          heap.mappedHeader(ptr0),
-          heap.mappedHeader(ptr1),
-          heap.mappedHeader(ptr2)
-        ];
-        headers.forEach((h, index) => {
-          // console.warn(`Checking header: ${index}...`);
-          expectFreeHeader(h, TAG_CLASS, 4n);
-        });
-      }
-      const ptr0new = mns.gcalloc(TAG_CLASS, 4n);
-      expect(Number(ptr0new)).to.equal(100);
-
-      expectFreeHeader(heap.heap.getHeader(112n), 0x0n as HeapTag, 0n);
-    });
-
-    // Simulates:
-    //
-    //   call(C(), C(), C())
-    it("temporary class allocate and sweep", () => {
-      const mns = new MnS(memory, heap);
-
-      mns.roots.captureTemps();
-      const ptr0 = mns.gcalloc(TAG_CLASS, 4n);
-      expect(Number(ptr0)).to.equal(100);
-
-      const ptr1 = mns.gcalloc(TAG_CLASS, 4n);
-      expect(Number(ptr1)).to.equal(104);
-
-      const ptr2 = mns.gcalloc(TAG_CLASS, 4n);
-      expect(Number(ptr2)).to.equal(108);
-
-      // Check that headers set correctly
-      {
-        const headers = [
-          heap.mappedHeader(ptr0),
-          heap.mappedHeader(ptr1),
-          heap.mappedHeader(ptr2)
-        ];
-        headers.forEach((h, index) => {
-          console.log(`Checking header: ${index}...`);
-          expectAllocatedHeader(h, TAG_CLASS, 4n);
-        });
-      }
-      mns.collect();
-      // Check that all temps are still allocated
-      {
-        const headers = [
-          heap.mappedHeader(ptr0),
-          heap.mappedHeader(ptr1),
-          heap.mappedHeader(ptr2)
-        ];
-        headers.forEach((h, index) => {
-          console.log(`Checking header: ${index}...`);
-          expectAllocatedHeader(h, TAG_CLASS, 4n);
-        });
-      }
-
-      mns.roots.releaseTemps();
-      mns.collect();
-      // Check that ptr0, ptr1, ptr2 is freed
-      {
-        const headers = [
-          heap.mappedHeader(ptr0),
-          heap.mappedHeader(ptr1),
-          heap.mappedHeader(ptr2)
-        ];
-        headers.forEach((h, index) => {
-          // console.warn(`Checking header: ${index}...`);
-          expectFreeHeader(h, TAG_CLASS, 4n);
-        });
-      }
-      const ptr0new = mns.gcalloc(TAG_CLASS, 4n);
-      expect(Number(ptr0new)).to.equal(100);
-
-      expectFreeHeader(heap.heap.getHeader(112n), 0x0n as HeapTag, 0n);
-    });
-
-    // Simulates:
-    //
-    //   x = C()
-    //   y = C()
-    //
-    //   y = C()
-    it("global var class allocate and sweep", () => {
-      const mns = new MnS(memory, heap);
-      const X_ADDR = 0x4n;
-      const Y_ADDR = 0x8n;
-
-      mns.roots.addGlobal(X_ADDR);
-      mns.roots.addGlobal(Y_ADDR);
-
-      mns.roots.captureTemps();
-      const ptr0 = mns.gcalloc(TAG_CLASS, 4n);
-      expect(Number(ptr0)).to.equal(100);
-      writeI32(memory, Number(X_ADDR), ptr0);
-
-      const ptr1 = mns.gcalloc(TAG_CLASS, 4n);
-      expect(Number(ptr1)).to.equal(104);
-      writeI32(memory, Number(Y_ADDR), ptr1);
-
-      const ptr2 = mns.gcalloc(TAG_CLASS, 4n);
-      expect(Number(ptr2)).to.equal(108);
-
-      // Check that headers set correctly
-      {
-        const headers = [
-          heap.mappedHeader(ptr0),
-          heap.mappedHeader(ptr1),
-          heap.mappedHeader(ptr2)
-        ];
-        headers.forEach((h, index) => {
-          console.log(`Checking header: ${index}...`);
-          expectAllocatedHeader(h, TAG_CLASS, 4n);
-        });
-      }
-
-      mns.roots.releaseTemps();
-      mns.collect();
-      // Check that global roots are not collected
-      {
-        const headers = [
-          heap.mappedHeader(ptr0),
-          heap.mappedHeader(ptr1),
-        ];
-        headers.forEach((h, index) => {
-          console.log(`Checking header: ${index}...`);
-          expectAllocatedHeader(h, TAG_CLASS, 4n);
-        });
-        expectFreeHeader(heap.mappedHeader(ptr2), TAG_CLASS, 4n);
-      }
-
-      const ptr3 = mns.gcalloc(TAG_CLASS, 4n);
-      expect(Number(ptr3)).to.equal(108);
-      // Overwrite y
-      writeI32(memory, Number(Y_ADDR), ptr3);
-      mns.collect();
-
-      // Check that global roots are not collected
-      {
-        const headers = [
-          heap.mappedHeader(ptr0),
-          heap.mappedHeader(ptr3),
-        ];
-        headers.forEach((h, index) => {
-          console.log(`Checking header: ${index}...`);
-          expectAllocatedHeader(h, TAG_CLASS, 4n);
-        });
-        expectFreeHeader(heap.mappedHeader(ptr1), TAG_CLASS, 4n);
-      }
-    });
+    // Need to give each test a blank slate
+    const cfgs: [Cfg, Cfg, Cfg] = [makeCfg(), makeCfg(), makeCfg()];
+    basicTests(cfgs);
   });
 });
 
+function basicTests(cfgs: [Cfg, Cfg, Cfg]) {
+
+  // Simulating:
+  // class C:
+  //   f: int
+  //
+  // def x():
+  //   x = C()
+  //   x = C()
+  //   y = C()
+  //
+  // f()
+  it(`[${cfgs[0].name}] local variable class allocate and sweep`, () => {
+    const heap = cfgs[0].heap;
+    const memory = cfgs[0].memory;
+    const mns = new MnS(memory, heap);
+
+    mns.roots.pushFrame();
+    const ptr0 = mns.gcalloc(TAG_CLASS, 4n);
+    expect(Number(ptr0)).to.equal(100);
+    mns.roots.addLocal(0n, ptr0);
+
+    const ptr1 = mns.gcalloc(TAG_CLASS, 4n);
+    expect(Number(ptr1)).to.equal(104);
+    mns.roots.addLocal(0n, ptr1);
+
+    const ptr2 = mns.gcalloc(TAG_CLASS, 4n);
+    expect(Number(ptr2)).to.equal(108);
+    mns.roots.addLocal(1n, ptr2);
+
+    // Check that headers set correctly
+    {
+      const headers = [
+        heap.mappedHeader(ptr0),
+        heap.mappedHeader(ptr1),
+        heap.mappedHeader(ptr2)
+      ];
+      headers.forEach((h, index) => {
+        console.log(`Checking header: ${index}...`);
+        expectAllocatedHeader(h, TAG_CLASS, 4n);
+      });
+    }
+
+    mns.collect();
+
+    // Check that only ptr0 is freed
+    {
+      const header0 = heap.mappedHeader(ptr0);
+      const header1 = heap.mappedHeader(ptr1);
+      const header2 = heap.mappedHeader(ptr2);
+
+      expectFreeHeader(header0, TAG_CLASS, 4n);
+      expectAllocatedHeader(header1, TAG_CLASS, 4n);
+      expectAllocatedHeader(header2, TAG_CLASS, 4n);
+    }
+
+    mns.roots.releaseLocals();
+    mns.collect();
+    // Check that ptr0, ptr1, ptr2 is freed
+    {
+      const headers = [
+        heap.mappedHeader(ptr0),
+        heap.mappedHeader(ptr1),
+        heap.mappedHeader(ptr2)
+      ];
+      headers.forEach((h, index) => {
+        // console.warn(`Checking header: ${index}...`);
+        expectFreeHeader(h, TAG_CLASS, 4n);
+      });
+    }
+    const ptr0new = mns.gcalloc(TAG_CLASS, 4n);
+    expect(Number(ptr0new)).to.equal(100);
+
+    expectFreeHeader(heap.heap.getHeader(112n), 0x0n as HeapTag, 0n);
+  });
+
+  // Simulates:
+  //
+  //   call(C(), C(), C())
+  it(`[${cfgs[1].name}] temporary class allocate and sweep`, () => {
+    const heap = cfgs[1].heap;
+    const memory = cfgs[1].memory;
+
+    const mns = new MnS(memory, heap);
+
+    mns.roots.captureTemps();
+    const ptr0 = mns.gcalloc(TAG_CLASS, 4n);
+    expect(Number(ptr0)).to.equal(100);
+
+    const ptr1 = mns.gcalloc(TAG_CLASS, 4n);
+    expect(Number(ptr1)).to.equal(104);
+
+    const ptr2 = mns.gcalloc(TAG_CLASS, 4n);
+    expect(Number(ptr2)).to.equal(108);
+
+    // Check that headers set correctly
+    {
+      const headers = [
+        heap.mappedHeader(ptr0),
+        heap.mappedHeader(ptr1),
+        heap.mappedHeader(ptr2)
+      ];
+      headers.forEach((h, index) => {
+        console.log(`Checking header: ${index}...`);
+        expectAllocatedHeader(h, TAG_CLASS, 4n);
+      });
+    }
+    mns.collect();
+    // Check that all temps are still allocated
+    {
+      const headers = [
+        heap.mappedHeader(ptr0),
+        heap.mappedHeader(ptr1),
+        heap.mappedHeader(ptr2)
+      ];
+      headers.forEach((h, index) => {
+        console.log(`Checking header: ${index}...`);
+        expectAllocatedHeader(h, TAG_CLASS, 4n);
+      });
+    }
+
+    mns.roots.releaseTemps();
+    mns.collect();
+    // Check that ptr0, ptr1, ptr2 is freed
+    {
+      const headers = [
+        heap.mappedHeader(ptr0),
+        heap.mappedHeader(ptr1),
+        heap.mappedHeader(ptr2)
+      ];
+      headers.forEach((h, index) => {
+        // console.warn(`Checking header: ${index}...`);
+        expectFreeHeader(h, TAG_CLASS, 4n);
+      });
+    }
+    const ptr0new = mns.gcalloc(TAG_CLASS, 4n);
+    expect(Number(ptr0new)).to.equal(100);
+
+    expectFreeHeader(heap.heap.getHeader(112n), 0x0n as HeapTag, 0n);
+  });
+
+  // Simulates:
+  //
+  //   x = C()
+  //   y = C()
+  //
+  //   y = C()
+  it(`[${cfgs[2].name}] global var class allocate and sweep`, () => {
+    const heap = cfgs[2].heap;
+    const memory = cfgs[2].memory;
+    const mns = new MnS(memory, heap);
+    const X_ADDR = 0x4n;
+    const Y_ADDR = 0x8n;
+
+    mns.roots.addGlobal(X_ADDR);
+    mns.roots.addGlobal(Y_ADDR);
+
+    mns.roots.captureTemps();
+    const ptr0 = mns.gcalloc(TAG_CLASS, 4n);
+    expect(Number(ptr0)).to.equal(100);
+    writeI32(memory, Number(X_ADDR), ptr0);
+
+    const ptr1 = mns.gcalloc(TAG_CLASS, 4n);
+    expect(Number(ptr1)).to.equal(104);
+    writeI32(memory, Number(Y_ADDR), ptr1);
+
+    const ptr2 = mns.gcalloc(TAG_CLASS, 4n);
+    expect(Number(ptr2)).to.equal(108);
+
+    // Check that headers set correctly
+    {
+      const headers = [
+        heap.mappedHeader(ptr0),
+        heap.mappedHeader(ptr1),
+        heap.mappedHeader(ptr2)
+      ];
+      headers.forEach((h, index) => {
+        console.log(`Checking header: ${index}...`);
+        expectAllocatedHeader(h, TAG_CLASS, 4n);
+      });
+    }
+
+    mns.roots.releaseTemps();
+    mns.collect();
+    // Check that global roots are not collected
+    {
+      const headers = [
+        heap.mappedHeader(ptr0),
+        heap.mappedHeader(ptr1),
+      ];
+      headers.forEach((h, index) => {
+        console.log(`Checking header: ${index}...`);
+        expectAllocatedHeader(h, TAG_CLASS, 4n);
+      });
+      expectFreeHeader(heap.mappedHeader(ptr2), TAG_CLASS, 4n);
+    }
+
+    const ptr3 = mns.gcalloc(TAG_CLASS, 4n);
+    expect(Number(ptr3)).to.equal(108);
+    // Overwrite y
+    writeI32(memory, Number(Y_ADDR), ptr3);
+    mns.collect();
+
+    // Check that global roots are not collected
+    {
+      const headers = [
+        heap.mappedHeader(ptr0),
+        heap.mappedHeader(ptr3),
+      ];
+      headers.forEach((h, index) => {
+        console.log(`Checking header: ${index}...`);
+        expectAllocatedHeader(h, TAG_CLASS, 4n);
+      });
+      expectFreeHeader(heap.mappedHeader(ptr1), TAG_CLASS, 4n);
+    }
+  });
+}
