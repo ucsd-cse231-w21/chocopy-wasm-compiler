a83b51db2902bf5fa64f25bfdc8a3fa2f724c2c5
Alexander Vo <amv036@ucsd.edu>
2021-03-10 17:34:27 -0800

    GC test simulator allocations within a function call

--- tests/gc-int.test.ts
+++ tests/gc-int.test.ts
@@ -60,12 +60,17 @@ class PhantomAllocator implements MarkableAllocator {
     }
 
     const header = this.heap.getHeader(result);
-    if (this.map.has(result)) {
-      throw new Error(`Double allocation at ${result}`);
-    }
     this.map.set(result, header);
 
     console.log(`Allocated ${size} at ${result} \t\t(tag=${tag}, header=${header.headerStart})`);
+    return result;
+  }
+
+  mappedHeader(ptr: BigInt): Header {
+    const result = this.map.get(ptr);
+    if (result === undefined) {
+      throw new Error(`No header mapped for ${ptr}`);
+    }
     return result;
   }
 
@@ -93,6 +98,42 @@ class PhantomAllocator implements MarkableAllocator {
   }
 }
 
+function readI32(memory: Uint8Array, start: number): bigint {
+  let x = BigInt.asUintN(32, 0x0n);
+
+  // WASM stores integers in little-endian:
+  //   LSB at the smallest address
+  for (let i = 0; i < 4; i++) {
+    const b = BigInt(memory[start + i]);
+    x = x + (b << BigInt(8 * i));
+  }
+
+  return x;
+}
+
+function writeI32(memory: Uint8Array, start: number, value: bigint) {
+  // WASM stores integers in little-endian:
+  //   LSB at the smallest address
+  for (let i = 0; i < 4; i++) {
+    const b = BigInt.asUintN(8, value >> BigInt(8 * i));
+    memory[start + i] = Number(b);
+  }
+}
+
+function expectAllocatedHeader(header: Header, tag: HeapTag, size: bigint) {
+  expect(header.isAlloced()).to.equal(true);
+  expect(header.isMarked()).to.equal(false);
+  expect(Number(header.getSize())).to.equal(Number(size));
+  expect(Number(header.getTag())).to.equal(Number(tag));
+}
+
+function expectFreeHeader(header: Header, tag: HeapTag, size: bigint) {
+  expect(header.isAlloced()).to.equal(false);
+  expect(header.isMarked()).to.equal(false);
+  expect(Number(header.getSize())).to.equal(Number(size));
+  expect(Number(header.getTag())).to.equal(Number(tag));
+}
+
 describe("MnS", () => {
   describe("MnS-BumpAllocator-1", () => {
     let memory: Uint8Array;
@@ -104,17 +145,73 @@ describe("MnS", () => {
       heap = new PhantomAllocator(bmb);
     });
 
+    // Simulating:
+    // class C:
+    //   f: int
+    //
+    // def x():
+    //   x = C()
+    //   x = C()
+    //   y = C()
+    //
+    // f()
     it("local variable class allocate and sweep", () => {
       const mns = new MnS(memory, heap);
 
+      mns.roots.pushFrame();
       const ptr0 = mns.gcalloc(TAG_CLASS, 4n);
       expect(Number(ptr0)).to.equal(100);
+      mns.roots.addLocal(0n, ptr0);
 
       const ptr1 = mns.gcalloc(TAG_CLASS, 4n);
       expect(Number(ptr1)).to.equal(104);
+      mns.roots.addLocal(0n, ptr1);
 
       const ptr2 = mns.gcalloc(TAG_CLASS, 4n);
       expect(Number(ptr2)).to.equal(108);
+      mns.roots.addLocal(1n, ptr2);
+
+      // Check that headers set correctly
+      {
+        const headers = [
+          heap.mappedHeader(ptr0),
+          heap.mappedHeader(ptr1),
+          heap.mappedHeader(ptr2)
+        ];
+        headers.forEach((h, index) => {
+          console.warn(`Checking header: ${index}...`);
+          expectAllocatedHeader(h, TAG_CLASS, 4n);
+        });
+      }
+
+      mns.collect();
+
+      // Check that only ptr0 is freed
+      {
+        const header0 = heap.mappedHeader(ptr0);
+        const header1 = heap.mappedHeader(ptr1);
+        const header2 = heap.mappedHeader(ptr2);
+
+        expectFreeHeader(header0, TAG_CLASS, 4n);
+        expectAllocatedHeader(header1, TAG_CLASS, 4n);
+        expectAllocatedHeader(header2, TAG_CLASS, 4n);
+      }
+
+      mns.roots.releaseLocals();
+      // Check that ptr0, ptr1, ptr2 is freed
+      {
+        const headers = [
+          heap.mappedHeader(ptr0),
+          heap.mappedHeader(ptr1),
+          heap.mappedHeader(ptr2)
+        ];
+        headers.forEach((h, index) => {
+          // console.warn(`Checking header: ${index}...`);
+          expectFreeHeader(h, TAG_CLASS, 4n);
+        });
+      }
+      const ptr0new = mns.gcalloc(TAG_CLASS, 4n);
+      expect(Number(ptr0new)).to.equal(100);
     });
   });
 });
