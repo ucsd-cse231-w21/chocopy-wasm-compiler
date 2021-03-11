7281f4f6e4172af091ff677da619c739a91fb6c3
Beichen Lyu <unbenson.lyu@gmail.com>
2021-03-11 12:33:20 -0800

    added numpy/list utils

--- utils.ts
+++ utils.ts
@@ -10,13 +10,15 @@ export function PyValue(typ: Type, result: number): Value {
       return PyObj(typ.name, result);
     case "none":
       return PyNone();
+    case "list":
+      return PyList(result);
     default:
       unhandledTag(typ);
   }
 }
 
 export function PyInt(n: number): Value {
-  return { tag: "num", value: BigInt(n) };
+  return { tag: "num", value: n };
 }
 
 export function PyBool(b: boolean): Value {
@@ -32,6 +34,11 @@ export function PyNone(): Value {
   return { tag: "none" };
 }
 
+// TODO: overwrite this by list team
+export function PyList(address: number): Value {
+  return { tag: "list", address, content_type: NUM };
+}
+
 export function isTagged<
   A extends string[],
   V extends { tag: string },
@@ -41,7 +48,7 @@ export function isTagged<
 }
 
 export function unreachable(arg: never): never {
-  throw new Error(`Hit unreachable state. Got value ${JSON.stringify(arg)}`);
+  throw new Error(`Hit unreachable state. Got value ${jsonStringify(arg)}`);
 }
 
 /**
@@ -49,7 +56,7 @@ export function unreachable(arg: never): never {
  * @param arg Tagged object which is not handled
  */
 export function unhandledTag(arg: { tag: string }): never {
-  throw new Error(`Node tagged with ${arg.tag} is not handled.\n\n${JSON.stringify(arg)}`);
+  throw new Error(`Node tagged with ${arg.tag} is not handled.\n\n${jsonStringify(arg)}`);
 }
 
 export const NUM: Type = { tag: "number" };
@@ -58,6 +65,7 @@ export const NONE: Type = { tag: "none" };
 export function CLASS(name: string): Type {
   return { tag: "class", name };
 }
+export const LIST: Type = { tag: "list", content_type: NUM }; // TODO: overwrite this by list team
 
 export function jsonStringify(data: any) : string {
   // JSON.stringify() with bigint support 
@@ -66,4 +74,7 @@ export function jsonStringify(data: any) : string {
       .replace(/"(-?\d+)#bigint"/g, (_, a) => a);
 }
 
-export const importPostfix : string = "$import";
+// special delimiters for importobjects call in ts
+// TODO: update these delimiters to be unique from python syntax
+export const importMethodDel : string = "_"; // eg numpy.mean <=> numpy_mean
+export const importDel : string = "_"; // eg numpy.ndarray <=> numpy_ndarray 
