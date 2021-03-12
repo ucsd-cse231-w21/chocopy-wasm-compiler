c4c8184b3d54cff51bca8ea0d05c3f71d797ac14
Beichen Lyu <unbenson.lyu@gmail.com>
2021-03-11 19:19:38 -0800

    added imported names in env

--- repl.ts
+++ repl.ts
@@ -27,6 +27,7 @@ export class BasicREPL {
       classes: new Map(),
       locals: new Set(),
       offset: 1,
+      imports: new Set(),
     };
     this.currentTypeEnv = defaultTypeEnv;
     this.functions = "";
