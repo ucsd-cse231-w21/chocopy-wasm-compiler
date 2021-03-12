1415075cc5f1b70eb3106d128dd9b388c6b3eadf
Freyr_Wings <freyrwings@gmail.com>
2021-03-12 16:12:34 +0800

    fix example

--- tests/inheritence2.test.ts
+++ tests/inheritence2.test.ts
@@ -2,51 +2,51 @@ import { PyBool, PyInt } from "../utils";
 import { assert, asserts, assertPrint, assertTCFail, assertTC, assertFail } from "./utils.test";
 
 describe('inheritence', () => {
-    assertPrint("constructor", `
+  assertPrint("constructor", `
 class animal(object):
-    number: int = 0
-    def __init__(self:animal):
-        self.number = 2
+  number: int = 0
+  def __init__(self:animal):
+    self.number = 2
 kit:animal = None
 kit = animal()
 print(kit.number)`, ["2"]);
 
-    assertPrint("constructor-with-param",`
+  assertPrint("constructor-with-param", `
 class animal(object):
-    number: int = 1
-    def __init__(self:animal, number:int):
-        self.number = number
+  number: int = 1
+  def __init__(self:animal, number:int):
+    self.number = number
 kit:animal = None
 kit = animal(2)
 print(kit.number)`, ["2"]);
 
-    assertPrint("attributes-of-base-class",`
+  assertPrint("attributes-of-base-class", `
 class animal(object):
-    number: int = 1
+  number: int = 1
         
 class cat(animal):
-    pass
+  pass
         
 kit:cat = None
 kit = cat()
 print(kit.number)`, ["1"]);
 
-    assertPrint("attributes-of-derived-class",`
+  assertPrint("attributes-of-derived-class", `
 class animal(object):
-    number: int = 1
+  number: int = 1
         
 class cat(animal):
-    eatFish: bool = True
+  eatFish: bool = True
         
 kit:cat = None
 kit = cat()
 print(kit.eatFish)`, ["True"]);
 
 
-    assertPrint("methods-of-base-class",`
+  assertPrint("methods-of-base-class", `
 class animal(object):
-    def run(self:animal):
-        print(1)
+  def run(self:animal):
+    print(1)
         
 class cat(animal):
     pass
@@ -55,71 +55,71 @@ kit:cat = None
 kit = cat()
 kit.run()`, ["1"]);
 
-    assertPrint("methods-of-derived-class",`
+  assertPrint("methods-of-derived-class", `
 class animal(object):
-    pass
+  pass
         
 class cat(animal):
-    def run(self:cat):
-        print(1)
+  def run(self:cat):
+    print(1)
         
 kit:cat = None
 kit = cat()
 kit.run()`, ["1"]);
 
-    assertPrint("hierarchy-of-inheritance",`
+  assertPrint("hierarchy-of-inheritance", `
 class animal(object):
-    number:int = 1
+  number:int = 1
 class mammal(animal):
-    leg:int = 4
+  leg:int = 4
 class cat(mammal):
-    def run(self:cat):
-        print(self.number+self.leg)
+  def run(self:cat):
+    print(self.number+self.leg)
         
 kit:cat = None
 kit = cat()
 kit.run()`, ["5"]);
 
-    assertPrint("re-defined-methods", `
+  assertPrint("re-defined-methods", `
 class animal(object):
-    def run(self:animal):
-        print(1)
+  def run(self:animal):
+    print(1)
         
 class cat(animal):
-    def run(self:cat):
-        print(2)
+  def run(self:cat):
+    print(2)
         
 kit:cat = None
 kit = cat()
 kit.run()`, ["2"]);
 
-    assertPrint("polymorphism1", `
+  assertPrint("polymorphism1", `
 class animal(object):
-    def run(self:animal):
-        print(1)
+  def run(self:animal):
+    print(1)
         
 class cat(animal):
-    pass
+  pass
 def f(pet:animal):
-    pet.run()
+  pet.run()
 
 kit:cat = None
 kit = cat()
 f(kit)`, ["1"]);
 
-    assertPrint("polymorphism2", `
+  assertPrint("polymorphism2", `
 class animal(object):
-    def run(self:animal):
-        pass
+  def run(self:animal):
+    pass
         
 class cat(animal):
-    def run(self:cat):
-        print(2)
+  def run(self:cat):
+    print(2)
 class dog(animal):
-    def run(self:dog):
-        print(1)
+  def run(self:dog):
+    print(1)
 def f(pet:animal):
-    pet.run()
+  pet.run()
 
 bob:dog = None
 kit:cat = None
@@ -128,18 +128,18 @@ kit = cat()
 f(kit)
 f(bob)`, ["2", "1"]);
 
-    assertPrint("extra1", `
+  assertPrint("extra1", `
 class cls1(object):
-    x:int=1
-    def __init__(c:cls1, x:int):
-        c.x=x*x
-    def run(c:cls1):
-        print(c.x)
+  x:int=1
+  def __init__(c:cls1, x:int):
+    c.x=x*x
+  def run(c:cls1):
+    print(c.x)
 
 class cls2(object):
-    x:cls1=None
-    def m(c:cls2):
-        c.x.run()
+  x:cls1=None
+  def m(c:cls2):
+    c.x.run()
 
 a:cls1=None
 b:cls2=None
@@ -148,17 +148,17 @@ b=cls2()
 b.x=a
 b.m()`, ["441"]);
 
-    assertPrint("extra2", `
+  assertPrint("extra2", `
 x:int=1
 def func1():
-    y:int=2
-    def func2():
-        z:int=3
-        print(x+y+z)
-    func2()
+  y:int=2
+  def func2():
+    z:int=3
+    print(x+y+z)
+  func2()
 func1()`, ["6"])
 
-    assert("resolve-issue", `
+  assert("resolve-issue", `
 class blah(object):
   x: int = 123
 
@@ -169,7 +169,7 @@ def test() -> blah:
     return blah()
 test().x`, PyInt(123));
 
-    assert("resolve-issue", `
+  assert("resolve-issue", `
 def test() -> bool:
   if True:
     return True
@@ -177,7 +177,7 @@ def test() -> bool:
   else:
     return False
 test()`, PyBool(true));
-    assert("inheritance init",`
+  assert("inheritance init", `
 class A(object):
   x:int = 10
   def __init__(self:A):
@@ -190,8 +190,8 @@ a:B = None
 a = B()
 a.x
 `
-,PyInt(12))
-    assert("more init",`
+    , PyInt(12))
+  assert("more init", `
 class A(object):
   x:int = 10
   def __init__(self:A):
@@ -204,59 +204,58 @@ class B(A):
 a:B = None
 a = B()
 a.x
-`
-,PyInt(13))
-    
-    assert("recursion", `
+`, PyInt(13))
+
+  assert("recursion", `
 class n(object):
   x: int = 10
   a: int = 0
   b: int = 1
   c: int = 0
 class fib(n):
-   def calfib(self:fib):
-     if (self.x > 0):
-       self.x = self.x - 1
-       self.c = self.a + self.b
-       self.a = self.b
-       self.b = self.c
-       self.calfib()
+  def calfib(self:fib):
+    if (self.x > 0):
+      self.x = self.x - 1
+      self.c = self.a + self.b
+      self.a = self.b
+      self.b = self.c
+      self.calfib()
 
 ans:fib = None
 ans = fib()
 ans.calfib()
 ans.b`, PyInt(89));
-    assert("polymorphism with recursion", `
+  assert("polymorphism with recursion", `
 class n(object):
   x: int = 10
   def calc(self:n):
     pass
 class fib(n):
-   a: int = 0
-   b: int = 1
-   c: int = 0
-   def calc(self:fib):
-     tmp: int = 0
-     tmp = self.x
-     if (self.x > 0):
-       self.x = self.x - 1
-       self.c = self.a + self.b
-       self.a = self.b
-       self.b = self.c
-       self.calc()
-     self.x = tmp
+  a: int = 0
+  b: int = 1
+  c: int = 0
+  def calc(self:fib):
+    tmp: int = 0
+    tmp = self.x
+    if (self.x > 0):
+      self.x = self.x - 1
+      self.c = self.a + self.b
+      self.a = self.b
+      self.b = self.c
+      self.calc()
+    self.x = tmp
 class exp2(n):
-   b: int = 1
-   def calc(self:exp2):
-     tmp: int = 0
-     tmp = self.x
-     if (self.x > 0):
-       self.b = self.b + self.b
-       self.x = self.x - 1
-       self.calc()
-     self.x = tmp
+  b: int = 1
+  def calc(self:exp2):
+    tmp: int = 0
+    tmp = self.x
+    if (self.x > 0):
+      self.b = self.b + self.b
+      self.x = self.x - 1
+      self.calc()
+    self.x = tmp
 def calc(ans:n):
-    ans.calc()
+  ans.calc()
 ans1:fib = None
 ans2:exp2 = None
 ans1 = fib()
@@ -264,39 +263,39 @@ calc(ans1)
 ans2 = exp2()
 calc(ans2)
 ans1.b+ans2.b`, PyInt(1113));
-    assert("initialization", `
+  assert("initialization", `
 class n(object):
   x: int = 10
   def __init__(self:n, i:int):
-     self.x = i
+    self.x = i
   def calc(self:n):
     pass
 class fib(n):
-   a: int = 0
-   b: int = 1
-   c: int = 0
-   def calc(self:fib):
-     tmp: int = 0
-     tmp = self.x
-     if (self.x > 0):
-       self.x = self.x - 1
-       self.c = self.a + self.b
-       self.a = self.b
-       self.b = self.c
-       self.calc()
-     self.x = tmp
+  a: int = 0
+  b: int = 1
+  c: int = 0
+  def calc(self:fib):
+    tmp: int = 0
+    tmp = self.x
+    if (self.x > 0):
+      self.x = self.x - 1
+      self.c = self.a + self.b
+      self.a = self.b
+      self.b = self.c
+      self.calc()
+    self.x = tmp
 class exp2(n):
-   b: int = 1
-   def calc(self:exp2):
-     tmp: int = 0
-     tmp = self.x
-     if (self.x > 0):
-       self.b = self.b + self.b
-       self.x = self.x - 1
-       self.calc()
-     self.x = tmp
+  b: int = 1
+  def calc(self:exp2):
+    tmp: int = 0
+    tmp = self.x
+    if (self.x > 0):
+      self.b = self.b + self.b
+      self.x = self.x - 1
+      self.calc()
+    self.x = tmp
 def calc(ans:n):
-    ans.calc()
+  ans.calc()
 ans1:fib = None
 ans2:exp2 = None
 ans1 = fib(3)
@@ -304,49 +303,15 @@ calc(ans1)
 ans2 = exp2(5)
 calc(ans2)
 ans1.b+ans2.b`, PyInt(35));
-    assert("while", `
+  assert("while", `
 a:int = 10
 s:int = 0
 while a > 0:
   s = s + a
   a = a - 1
 s`, PyInt(55));
-/*
-    assertPrint("mutual recursion",`
 
-class n(object):
-  x:int = 10
-  y:int = 10
-  def __init__(self:n, i:int, j:int):
-    self.x = i
-    self.y = j
-
-class ack(n):
-  def work(self:ack)->int:
-    tmp:ack2 = None
-    tmp2:ack2 = None
-    k:int = 0
-    if(self.x == 0):
-        return self.y + 1
-    if(self.y == 0):
-        tmp = ack2(self.x - 1, 1)
-        return tmp.work()
-    tmp2 = ack2(self.x, self.y - 1)
-    tmp = ack2(0, 0)
-    tmp.x = self.x - 1
-    tmp.y = tmp2.work()     
-    return tmp.work()      
-
-class ack2(n):
-  def work(self:ack2)->int:
-    tmp:ack = None
-    return self.x
-a:ack = None
-b:ack2 = None
-a = ack(2, 2)
-b = ack2(2, 2)
-print(a.work() == b.work())`, ["True"])*/
-    assertPrint("mutual recursion",`
+  assertPrint("mutual recursion", `
 class n(object):
   x:int = 10
   y:int = 10
@@ -388,26 +353,23 @@ a:ack = None
 b:ack2 = None
 a = ack(2, 2)
 b = ack2(2, 2)
-print(a.work() == b.work())
-`, ["True"])
-assertTCFail("wrong base name", `
+print(a.work() == b.work())`, ["True"])
+  assertTCFail("wrong base name", `
 class C(object):
   x:int = 0
 class D(c):
   x:bool = False
 d:D = None
-d = D()
-  `);
-assertTCFail("wrong attr type", `
+d = D()`);
+  assertTCFail("wrong attr type", `
 class C(object):
   x:int = 0
 class D(C):
   x:bool = False
 d:D = None
 d = D()
-D().x+1
-  `);
-assertTCFail("wrong method type", `
+D().x+1`);
+  assertTCFail("wrong method type", `
 class C(object):
   x:int = 0
   def work(self:C)->int:
@@ -420,6 +382,4 @@ d:D = None
 d = D()
 d.work()+1
   `);
-
-
 });
